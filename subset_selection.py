e99597987d682394633c46071100ed4720a02384
elias <eliastoukolehto@gmail.com>
2025-12-11 20:27:27 +0200

    add multiclass feature selection

--- subset_selection.py
+++ subset_selection.py
@@ -25,67 +25,75 @@ train_y2 = train_y2.replace('Ia', 'event')
 train_y2 = train_y2.replace('Ib', 'event')
 train_y2 = train_y2.replace('II', 'event')
     
-def nb_ig(nb, x, y):
-    res = permutation_importance(nb, x, y, n_repeats=5, random_state=0, scoring='accuracy')
-    return res.importances_mean
 
 clf_lasso = LogisticRegression(random_state=0, solver='saga', penalty='l1', C=1, tol=0.001, max_iter=1000)
 clf_ridge = LogisticRegression(random_state=0, solver='saga', penalty='l2', C=1, tol=0.001, max_iter=1000)
 clf_DT = DecisionTreeClassifier()
 #clf_NB = GaussianNB()
 
-sel1 = RFECV(clf_lasso, min_features_to_select=1, step=1)
-sel1.fit(train_x, train_y2)
+def subset_selection(y):
 
-sel2 = RFECV(clf_ridge, min_features_to_select=1, step=1)
-sel2.fit(train_x, train_y2)
+    sel1 = RFECV(clf_lasso, min_features_to_select=1, step=1)
+    sel1.fit(train_x, y)
 
-sel4 = RFECV(clf_DT, min_features_to_select=1, step=1)
-sel4.fit(train_x, train_y2)
+    sel2 = RFECV(clf_ridge, min_features_to_select=1, step=1)
+    sel2.fit(train_x, y)
 
-""" sel3 = RFECV(clf_NB, min_features_to_select=1, step=1, importance_getter=nb_ig)
-sel3.fit(train_x, train_y2) """
+    sel3 = RFECV(clf_DT, min_features_to_select=1, step=1)
+    sel3.fit(train_x, y)
 
-
-#different outputs
-""" print('CV_results:', sel1.cv_results_)
-print('feature_names:', sel1.feature_names_in_)
-print('number of features:', sel1.n_features_)
-print('number of features during fit:', sel1.n_features_in_)
-print(sel1.ranking_) """
+    """ sel4 = RFECV(clf_NB, min_features_to_select=1, step=1, importance_getter=nb_ig)
+    sel4.fit(train_x, train_y2) """
 
 
-selections = [sel1, sel2, sel4]
-
-cv_results = []
-opt_features = []
-opt_accuracy = []
-avg_accuracy = []
-
-for sel in selections:
-    data = {
-        key: value
-        for key, value in sel.cv_results_.items()
-        if key in ["n_features", "mean_test_score", "std_test_score"]
-    }
-    cv_results.append(pd.DataFrame(data))
-    opt_features.append(sel.n_features_)
-    opt_accuracy.append(data["mean_test_score"][sel.n_features_])
-    avg_accuracy.append(data["mean_test_score"][99])
-
-model = ['LR L1', 'LR L2', 'DT']
-
-RFE_df = pd.DataFrame({ 'Model': model, 'Optimal n of features': opt_features, 'Best accuracy': opt_accuracy, 'Accuracy without RFE': avg_accuracy})
-RFE_df.set_index('Model')
+    #different outputs
+    """ print('CV_results:', sel1.cv_results_)
+    print('feature_names:', sel1.feature_names_in_)
+    print('number of features:', sel1.n_features_)
+    print('number of features during fit:', sel1.n_features_in_)
+    print(sel1.ranking_) """
 
 
-""" print(RFE_df)
+    selections = [sel1, sel2, sel3]
 
-LR1_results = cv_results[0].iloc[:40]
-LR2_results = cv_results[1].iloc[:40]
-DT_results = cv_results[2].iloc[:40]
+    cv_results = []
+    opt_features = []
+    opt_accuracy = []
+    avg_accuracy = []
+
+    for sel in selections:
+        data = {
+            key: value
+            for key, value in sel.cv_results_.items()
+            if key in ["n_features", "mean_test_score", "std_test_score"]
+        }
+        cv_results.append(pd.DataFrame(data))
+        opt_features.append(sel.n_features_)
+        opt_accuracy.append(data["mean_test_score"][sel.n_features_])
+        avg_accuracy.append(data["mean_test_score"][99])
+
+    model = ['LR L1', 'LR L2', 'DT']
+
+    RFE_df = pd.DataFrame({ 'Model': model, 'Optimal n of features': opt_features, 'Best accuracy': opt_accuracy, 'Accuracy without RFE': avg_accuracy})
+    RFE_df.set_index('Model')
+
+    return RFE_df, cv_results
+
+RFE_df_binary, cv_results_binary = subset_selection(train_y2)
+RFE_df_multi, cv_results_multi = subset_selection(train_y)
+
+""" print(RFE_df_binary)
+print(RFE_df_multi)
+
+LR1_results = cv_results_binary[0].iloc[:40]
+LR2_results = cv_results_binary[1].iloc[:40]
+DT_results = cv_results_binary[2].iloc[:40]
+
+LR1_results2 = cv_results_multi[0].iloc[:40]
+LR2_results2 = cv_results_multi[1].iloc[:40]
+DT_results2 = cv_results_multi[2].iloc[:40]
 #NB_results = cv_results[2].iloc[:40]
-
+plt.subplot(1,2,1)
 plt.plot(LR1_results["n_features"], LR1_results["mean_test_score"], color='red', label='LR (Lasso)')
 plt.plot(LR2_results["n_features"], LR2_results["mean_test_score"], color='blue', label='LR (Ridge)')
 plt.plot(DT_results["n_features"], DT_results["mean_test_score"], color='green', label='Decicion Tree')
@@ -93,4 +101,14 @@ plt.plot(DT_results["n_features"], DT_results["mean_test_score"], color='green',
 
 plt.legend()
 plt.title('RFE binary classification accuracy')
+
+plt.subplot(1,2,2)
+plt.plot(LR1_results2["n_features"], LR1_results2["mean_test_score"], color='red', label='LR (Lasso)')
+plt.plot(LR2_results2["n_features"], LR2_results2["mean_test_score"], color='blue', label='LR (Ridge)')
+plt.plot(DT_results2["n_features"], DT_results2["mean_test_score"], color='green', label='Decicion Tree')
+#plt.plot(NB_results["n_features"], NB_results["mean_test_score"], color='green', label='Naive Bayes')
+
+plt.legend()
+plt.title('RFE multiclass accuracy')
+
 plt.show() """
